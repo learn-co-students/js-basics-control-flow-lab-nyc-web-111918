7d38d59a5c2042ba0d1a7fb75dfefdd0e4f24bb8
lumrachele <rlum894@gmail.com>
2019-01-05 23:48:38 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your code in this file!
+function scuberGreetingForFeet(someValue){
+
+  let result;
+  if (someValue <= 400){
+    return result = 'This one is on me!';
+  }
+  else if (someValue > 2000 && someValue <2500){
+    return result = 'I will gladly take your thirty bucks.';
+  }
+  else if (someValue >2500){
+    return result = 'No can do.';
+  }
+}
+
+function ternaryCheckCity(city){
+  let result;
+  return result = city==="NYC"? 'Ok, sounds good.':'No go.';
+}
+
+function switchOnCharmFromTip(tip){
+  let result;
+  switch(tip){
+    case 'generous':
+      return result = 'Thank you so much.';
+      break;
+    case 'not as generous':
+      return result = 'Thank you.';
+      break;
+    default:
+      return result = 'Bye.';
+      break;
+  }
+}
