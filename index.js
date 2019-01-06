842d865460cdc67d1001a6804f59c7c2c223db04
isaac <oseasisaac91@gmail.com>
2019-01-05 21:13:09 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your code in this file!
+function scuberGreetingForFeet(distance) {
+  if (distance <= 400){
+    return 'This one is on me!'
+  }
+  else if (distance > 2500){
+    return 'No can do.'
+  }
+  else if (distance > 2000){
+    return 'I will gladly take your thirty bucks.'
+  }
+}
+
+function ternaryCheckCity(city) {
+  let response;
+  city === "NYC" ? (response = 'Ok, sounds good.') : (response = 'No go.')
+  return response
+}
+
+function switchOnCharmFromTip(tip) {
+  let response;
+
+  switch (tip) {
+    case 'generous':
+      response = "Thank you so much."
+      break
+    case 'not as generous':
+      response = 'Thank you.'
+      break
+    default:
+      response = 'Bye.'
+  };
+  return response
+}
